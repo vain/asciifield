3c15869e0966b71cd8372636dae956d79e885cea
Peter Hofmann <scm@uninformativ.de>
2015-02-26 14:29:22 +0100

    Some comments :-)

--- asciifield.c
+++ asciifield.c
@@ -53,11 +53,13 @@ init(struct screen *s)
         exit(EXIT_FAILURE);
     }
 
+    /* Clipping planes, unused aspect ratio, FOV 45 degree. */
     s->n = 1;
     s->f = 10;
     s->aspect = 1;
     s->theta = 45 * DEG_2_RAD;
 
+    /* Initialize standard OpenGL-ish projection matrix. */
     i = 0;
     s->m[i++] = 1.0 / tan(s->theta * 0.5 * s->aspect);
     s->m[i++] = 0;
@@ -79,6 +81,7 @@ init(struct screen *s)
     s->m[i++] = -(2 * s->n * s->f) / (s->f - s->n);
     s->m[i++] = 0;
 
+    /* Misc options. */
     s->speed = 3;
     s->fps = 30;
     s->num_stars = 300;
@@ -97,9 +100,11 @@ draw(struct screen *s, double *v_orig, double *v)
     double x_p, y_p, len2;
     char c;
 
+    /* Clipping. */
     if (v[2] >= 0)
         return;
 
+    /* Set "character size" depending on distance to camera. */
     len2 = v_orig[0] * v_orig[0] + v_orig[1] * v_orig[1] + v_orig[2] * v_orig[2];
     if (len2 > 50)
         c = '.';
@@ -108,6 +113,7 @@ draw(struct screen *s, double *v_orig, double *v)
     else
         c = '@';
 
+    /* Scale to screen pixels. */
     x_p = (v[0] + 1) * 0.5 * s->width;
     y_p = (v[1] + 1) * 0.5 * s->height;
 
@@ -123,9 +129,10 @@ show(struct screen *s)
     for (y = 0; y < s->height; y++)
     {
         for (x = 0; x < s->width; x++)
-        {
             printf("%c", s->fb[y * s->width + x]);
-        }
+
+        /* Do not print a newline in the very last line to avoid
+         * flickering. */
         if (y + 1 < s->height - 1)
             printf("\n");
     }
@@ -134,15 +141,18 @@ show(struct screen *s)
 void
 project(struct screen *s, double *v, double *v_p)
 {
+    /* Project vector. */
     v_p[0] = v[0] * s->m[0] + v[1] * s->m[4] + v[2] * s->m[8] + v[3] * s->m[12];
     v_p[1] = v[0] * s->m[1] + v[1] * s->m[5] + v[2] * s->m[9] + v[3] * s->m[13];
     v_p[2] = v[0] * s->m[2] + v[1] * s->m[6] + v[2] * s->m[10] + v[3] * s->m[14];
     v_p[3] = v[0] * s->m[3] + v[1] * s->m[7] + v[2] * s->m[11] + v[3] * s->m[15];
 
+    /* "Dehomogenize". */
     v_p[0] /= v_p[3];
     v_p[1] /= v_p[3];
     v_p[2] /= v_p[3];
 
+    /* Divide by z. */
     v_p[0] /= v_p[2];
     v_p[1] /= v_p[2];
 }
@@ -162,23 +172,32 @@ cleanup_stars(struct screen *s, struct star **field)
 {
     struct star *p, *prev = NULL;
 
+    /* If a star is outside the frustum, then remove it and make room
+     * for a new one. */
+
     for (p = *field; p != NULL; p = p->next)
     {
         if (p->v[2] < s->n || p->v[2] > s->f)
         {
             if (p == *field)
             {
+                /* Delete very first star, hence update the "public"
+                 * pointer. */
                 *field = (*field)->next;
                 free(p);
+
                 p = *field;
             }
             else
             {
+                /* Deletion of any other star. We must first find the
+                 * last list element. */
                 for (prev = *field; prev->next != p; prev = prev->next)
                     /* noop */ ;
 
                 prev->next = p->next;
                 free(p);
+
                 p = prev;
             }
         }
@@ -193,11 +212,13 @@ ensure_stars(struct screen *s, struct star **field)
 
     if (*field == NULL)
     {
+        /* Create an initial list element if there is none yet. */
         srand48(time(0));
         *field = malloc(sizeof(struct star));
         random_star(s, *field, s->first);
     }
 
+    /* Count existing stars. */
     existing = 0;
     for (p = *field; p != NULL; p = p->next)
     {
@@ -205,6 +226,7 @@ ensure_stars(struct screen *s, struct star **field)
         existing++;
     }
 
+    /* Create missing stars. */
     while (existing < s->num_stars)
     {
         p = malloc(sizeof(struct star));
@@ -214,6 +236,8 @@ ensure_stars(struct screen *s, struct star **field)
         existing++;
     }
 
+    /* Only the very first pass will create stars all over the place.
+     * All subsequent passes will create stars at z = far. */
     s->first = 0;
 }
 
@@ -250,6 +274,7 @@ main()
 
     init(&s);
 
+    /* Hide cursor and restore it when we're exiting. */
     printf("\033[?25l");
 	signal(SIGINT, cleanup_terminal);
 	signal(SIGHUP, cleanup_terminal);
@@ -259,6 +284,9 @@ main()
 
     while (1)
     {
+        /* Jump back to top left corner. Do not clear anything. Each
+         * pixel will be overwritten one at a time. This avoids
+         * flickering. */
         printf("\033[H");
 
         cleanup_stars(&s, &field);
@@ -273,6 +301,8 @@ main()
 
         show(&s);
 
+        /* Depending on how much time has passed, calculate the required
+         * step size to get the configured speed. */
         gettimeofday(&t2, NULL);
         stepsize = calc_stepsize(&s, &t1, &t2);
         t1 = t2;
@@ -280,6 +310,8 @@ main()
         for (p = field; p != NULL; p = p->next)
             p->v[2] -= stepsize;
 
+        /* This is just an upper limit. If we're doing less fps, the
+         * speed will remain the same. */
         usleep((1.0 / s.fps) * 1e6);
     }
 }
